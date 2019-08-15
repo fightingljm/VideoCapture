e8afb36c5945c18e8ae7592238df9fd3b4902727
刘金萌 <1321839748ljm@gmail.com>
2019-08-15 09:49:38 +0800

    first commit

---
+++ VideoCapture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoCapture
+//
+//  Created by 刘金萌 on 2019/8/15.
+//  Copyright © 2019 刘金萌. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
