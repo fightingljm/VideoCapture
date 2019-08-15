6a52c5dbd30404d01df46f71f30bdd88264b45aa
刘金萌 <1321839748ljm@gmail.com>
2019-08-15 11:27:41 +0800

    setupVideo

--- VideoCapture/ViewController.swift
+++ VideoCapture/ViewController.swift
@@ -19,30 +19,14 @@ class ViewController: UIViewController {
 extension ViewController {
     
     @IBAction func startCapture(_ sender: Any) {
+        // 1.设置音频的输入&输出
+        setupVideo()
         
-        // 1.创建捕捉会话
-        
-        // 2.给捕捉会话创建输入源(摄像头)
-        // 获取摄像头设备
-        guard let devices = AVCaptureDevice.devices(for: AVMediaType.video) as? [AVCaptureDevice] else {
-            print("摄像头不可用")
-        }
-        guard let device = devices.filter({ $0.position == .front }).first else { return }
-        // 通过device创建AVCaptureInput对象
-        guard let videoInput = try? AVCaptureDeviceInput(device: device) else { return }
-        // 将input添加到会话中
-        session.addInput(videoInput)
-        
-        // 3.给捕捉会话创建输出源
-        let videoOutput = AVCaptureVideoDataOutput()
-        videoOutput.setSampleBufferDelegate(self, queue: videoQueue)
-        session.addOutput(videoOutput)
-        
-        // 4.给用户创建一个预览图层（可选）
+        // 2.给用户创建一个预览图层（可选）
         previewLayer.frame = view.bounds
         view.layer.insertSublayer(previewLayer, at: 0)
         
-        // 5.开始采集
+        // 3.开始采集
         session.startRunning()
         
     }
@@ -54,6 +38,27 @@ extension ViewController {
     
 }
 
+extension ViewController {
+    fileprivate func setupVideo() {
+        // 1.给捕捉会话创建输入源(摄像头)
+        // 1.1获取摄像头设备
+        guard let devices = AVCaptureDevice.devices(for: AVMediaType.video) as? [AVCaptureDevice] else {
+            print("摄像头不可用")
+        }
+        guard let device = devices.filter({ $0.position == .front }).first else { return }
+        // 1.2通过device创建AVCaptureInput对象
+        guard let videoInput = try? AVCaptureDeviceInput(device: device) else { return }
+        // 1.3将input添加到会话中
+        session.addInput(videoInput)
+        
+        // 2.给捕捉会话创建输出源
+        let videoOutput = AVCaptureVideoDataOutput()
+        videoOutput.setSampleBufferDelegate(self, queue: videoQueue)
+        session.addOutput(videoOutput)
+    }
+}
+
+// MARK:- 获取数据
 extension ViewController : AVCaptureVideoDataOutputSampleBufferDelegate{
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         print("已经采集到画面")
