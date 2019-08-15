92720c011b195437668e902f88da0b8a4f36bdc6
刘金萌 <1321839748ljm@gmail.com>
2019-08-15 13:13:24 +0800

    采集音频

--- VideoCapture/ViewController.swift
+++ VideoCapture/ViewController.swift
@@ -11,22 +11,29 @@ import AVFoundation
 
 class ViewController: UIViewController {
     fileprivate lazy var videoQueue = DispatchQueue.global()
+    fileprivate lazy var audioQueue = DispatchQueue.global()
+    
     fileprivate lazy var session: AVCaptureSession = AVCaptureSession()
     fileprivate lazy var previewLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: self.session)
+    
+    fileprivate var connection : AVCaptureConnection?
 }
 
 // MARK:- 视频的开始采集&停止采集
 extension ViewController {
     
     @IBAction func startCapture(_ sender: Any) {
-        // 1.设置音频的输入&输出
+        // 1.设置视频的输入&输出
         setupVideo()
         
-        // 2.给用户创建一个预览图层（可选）
+        // 2.设置音频的输入&输出
+        setupAudio()
+        
+        // 3.给用户创建一个预览图层（可选）
         previewLayer.frame = view.bounds
         view.layer.insertSublayer(previewLayer, at: 0)
         
-        // 3.开始采集
+        // 4.开始采集
         session.startRunning()
         
     }
@@ -40,7 +47,7 @@ extension ViewController {
 
 extension ViewController {
     fileprivate func setupVideo() {
-        // 1.给捕捉会话创建输入源(摄像头)
+        // 1.给捕捉会话创建输入源(摄像头📹)
         // 1.1获取摄像头设备
         guard let devices = AVCaptureDevice.devices(for: AVMediaType.video) as? [AVCaptureDevice] else {
             print("摄像头不可用")
@@ -55,12 +62,32 @@ extension ViewController {
         let videoOutput = AVCaptureVideoDataOutput()
         videoOutput.setSampleBufferDelegate(self, queue: videoQueue)
         session.addOutput(videoOutput)
+        
+        // 3.获取video对应的connection
+        connection = videoOutput.connection(with: AVMediaType.video)
+    }
+    fileprivate func setupAudio() {
+        // 1.设置音频的输入（话筒🎙️）
+        guard let device = AVCaptureDevice.default(for: AVMediaType.audio) else { return }
+        
+        guard let audioInput = try? AVCaptureDeviceInput(device: device) else { return }
+        
+        session.addInput(audioInput)
+        
+        let audioOutput = AVCaptureAudioDataOutput()
+        audioOutput.setSampleBufferDelegate(self, queue: audioQueue)
+        session.addOutput(audioOutput)
+        
     }
 }
 
 // MARK:- 获取数据
-extension ViewController : AVCaptureVideoDataOutputSampleBufferDelegate{
+extension ViewController : AVCaptureVideoDataOutputSampleBufferDelegate, AVCaptureAudioDataOutputSampleBufferDelegate{
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        print("已经采集到画面")
+        if connection == self.connection {
+            print("采集到视频画面")
+        }else {
+            print("采集到音频")
+        }
     }
 }
